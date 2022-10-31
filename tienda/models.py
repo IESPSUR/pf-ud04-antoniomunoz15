3f71179ee5337c9b2d914c85658c0bca77720ca1
antoniomunoz15 <antonio.munoz.reinoso.al@iespoligonosur.org>
2022-10-31 09:59:31 +0100

    Producto

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Producto(models.Model):
+    modelo = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    unidades = models.IntegerField()
+    detalles = models.CharField(max_length=500)
