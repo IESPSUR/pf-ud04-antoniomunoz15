68daa3a81ae3b7e71204c5e8ecf5d3370422e847
antoniomunoz15 <antonio.munoz.reinoso.al@iespoligonosur.org>
2022-11-13 13:59:36 +0100

    Crudsv2

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('tienda/', views.welcome, name='welcome'),
-    path('tienda/listado', views.listado, name='listado'),
-    path('tienda/edicion', views.edicion, name='edicion'),
-    path('tienda/edicion/<int:id>', views.edicion, name='edicion'),
-    path('tienda/nuevo', views.nuevo, name='nuevo'),
-    path('tienda/eliminar/<int:id>', views.eliminar, name='eliminar'),
+    path('tienda/admin/listado', views.listado, name='listado'),
+    path('tienda/admin/edicion', views.edicion, name='edicion'),
+    path('tienda/admin/edicion/<int:id>', views.edicion, name='edicion'),
+    path('tienda/admin/nuevo', views.nuevo, name='nuevo'),
+    path('tienda/admin/eliminar/<int:id>', views.eliminar, name='eliminar'),
 ]
